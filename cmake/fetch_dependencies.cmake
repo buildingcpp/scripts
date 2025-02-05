9d14ef889b650cd2584b48f5a87739d6bbd479c6
Michael Maniscalco <michael@buildingcpp.com>
2025-02-05 16:14:22 -0500

    remove empty install_command string to avoid annoying cmake warnings

--- cmake/fetch_dependencies.cmake
+++ cmake/fetch_dependencies.cmake
@@ -21,7 +21,6 @@ function(fetch_dependency dependency)
             SOURCE_DIR        "${CMAKE_BINARY_DIR}/${repo_name}-src"
             BINARY_DIR        "${CMAKE_BINARY_DIR}/${repo_name}-build"
             INSTALL_DIR       "${CMAKE_BINARY_DIR}"
-            INSTALL_COMMAND   ""
             )
         FetchContent_MakeAvailable(${repo_name})
         FetchContent_GetProperties(${repo_name})
